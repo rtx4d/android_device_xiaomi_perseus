cb7320fb0fe781f435cf5871ca178b79d371f3a9
Alexander Koskovich <zvnexus@outlook.com>
2021-03-16 19:15:00 +0700

    perseus: Define BOARD_AVB_RECOVERY_KEY_PATH.
     * BOARD_AVB_RECOVERY_KEY_PATH must be defined if non-A/B is supported.
       See https://android.googlesource.com/platform/external/avb/+/master/README.md#booting-into-recovery.
    
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,5 +44,10 @@ TARGET_USES_NQ_NFC := true
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 2
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/perseus/BoardConfigVendor.mk
