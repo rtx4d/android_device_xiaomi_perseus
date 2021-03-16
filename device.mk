f43e2182a4bfd8df656535204132319fefa99cd8
Andrey Sprynga <rtx4d@aospa.co>
2021-03-16 14:35:25 +0700

    perseus: Drop lineage DeviceSettings
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- device.mk
+++ device.mk
@@ -96,10 +96,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     SecureElement
 
-# Slider settings
-PRODUCT_PACKAGES += \
-    DeviceSettings
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     vendor/nxp/opensource/pn5xx
