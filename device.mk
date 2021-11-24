598fef86bc0c597a7a9dac6a808507329b5e3f38
Chenyang Zhong <zhongcy95@gmail.com>
2021-11-24 12:01:07 +0700

    perseus: turn off compressed apex
    Google introduced compressed apex on android 12 [1], and the flag is
    enabled by default in updatable_apex.mk [2]. Polaris cannot boot
    compressed apex with encryption, so disable it.
    
    ref:
    [1] https://source.android.com/devices/tech/ota/apex#compressed-apex
    [2] https://android.googlesource.com/platform/build/+/4a1240de8a2ee5795a30df123058677d3999145e
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- device.mk
+++ device.mk
@@ -9,6 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/perseus/perseus-vendor.mk)
